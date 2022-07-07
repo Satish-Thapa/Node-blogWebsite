9bd44938ab6a43f197dfe3a6f4245889d2ba643f
Satish Thapa <you@example.com>
2022-07-07 16:27:42 +0545

    V2

--- app.js
+++ app.js
@@ -3,16 +3,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-let content = {};
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/blogsDB");
+
 
 const _ = require('lodash');
 
-let contents = [];
+
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
+
+const blogSchema = new mongoose.Schema({
+  name: String,
+  blog : String
+});
+
+const Blog = mongoose.model("Blod",blogSchema);
+
+const blog1 = new Blog({
+  name: "home",
+  blog: homeStartingContent
+});
+
+// blog1.save();
+
+let contents = [blog1];
+
+// Blog.insertMany([contents],function(err){
+//   if(!err){
+//     console.log(err);
+//   } else {
+//     console.log("Sucessfultt added data");
+
+//   }
+// });
+
 
 const app = express();
 
@@ -23,7 +55,21 @@ app.use(express.static("public"));
 
 
 app.get("/",function(req,res){
-  res.render("home",{homeStartingContent: homeStartingContent, contents:contents});
+  var homeBlog = "";
+  Blog.find(function(err,result){
+    console.log("aani ma bhaye");
+    if(!err){
+      homeBlog = result[0].blog;
+      console.log(homeBlog);
+      res.render("home",{homeStartingContent: homeBlog, contents:contents});
+    } else {
+      console.log(err);
+  
+    }
+
+  });
+ 
+  
 });
 
 app.get("/about",function(req,res){
@@ -46,11 +92,21 @@ app.get("/posts/:day",function(req,res){
   });
 });
 
+const composeSchema = new mongoose.Schema({
+ title : String,
+ blog : String
+})
+
+const Compose = mongoose.model("Compose",composeSchema);
+
 app.post("/compose",function(req,res){
   content = {
+    
+  }
+  const comp1 = new Compose({
     title: req.body.postTitle,
     blog: req.body.postBody
-  }
+  })
   contents.push(content);
   res.redirect("/");
   
